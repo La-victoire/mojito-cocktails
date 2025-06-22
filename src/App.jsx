46c5d3ec2179f6c7b64dfe3726ea4a1dd7ea866b
la_victoire <oghuvbuvictory@gmail.com>
2025-06-22 01:32:22 +0100

    new file:   constants/index.js 	modified:   src/App.jsx 	new file:   src/components/Cocktail.jsx 	new file:   src/components/Hero.jsx 	new file:   src/components/Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktail from './components/Cocktail';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
-    <div className='flex-center h-[100dvh]'>
-      <h1 className='text-3xl text-indigo-300'>Hello World</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktail />
+    </main>
   )
 }
 
